855bbe4074b2a562e5ca63fd1cfa7b95a8d66d2b
Konrad Boryś <konrad.borys@outlook.com>
2022-07-17 11:46:21 +0200

    Added db, table and stored procedures

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,7 @@
+﻿IF not exists(SELECT 1 FROM dbo.[User])
+BEGIN
+	INSERT INTO dbo.[User] (FirstName, LastName)
+	VALUES ('Konrad', 'Boryś'),
+	('Zbigniew', 'Wodecki'),
+	('Krzysztof', 'Krawczyk');
+END
---
+++ UserDB/dbo/StoredProcedures/spUser_Delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Delete]
+	@Id int
+AS
+BEGIN
+	delete
+	from dbo.[User]
+	where Id = @Id
+END
